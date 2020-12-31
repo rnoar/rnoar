3a4a731fc0134dbdbaa602241f0f2e99bb4b3445
razinkovnik <51551350+razinkovnik@users.noreply.github.com>
2020-12-31 13:30:43 +0300

    seo

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,6 +22,11 @@ export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'РНОАР',
+    script: [
+      {
+        src: 'https://www.googletagmanager.com/gtag/js?id=G-6MSWZRHBDD'
+      }
+    ],
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
--- plugins/ga.js
+++ plugins/ga.js
@@ -8,37 +8,11 @@ export default ({ app }) => {
       return /*
        ** Include Google Analytics Script
        */
-    ;(function (i, s, o, g, r, a, m) {
-      i['GoogleAnalyticsObject'] = r
-      ;(i[r] =
-        i[r] ||
-        function () {
-          ;(i[r].q = i[r].q || []).push(arguments)
-        }),
-        (i[r].l = 1 * new Date())
-      ;(a = s.createElement(o)), (m = s.getElementsByTagName(o)[0])
-      a.async = 1
-      a.src = g
-      m.parentNode.insertBefore(a, m)
-    })(
-      window,
-      document,
-      'script',
-      'https://www.google-analytics.com/analytics.js',
-      'ga'
-    )
-    /*
-     ** Set the current page
-     */
-    ga('create', 'G-6MSWZRHBDD', 'auto')
-    /*
-     ** Every time the route changes (fired on initialization too)
-     */
-    app.router.afterEach((to, from) => {
-      /*
-       ** We tell Google Analytics to add a `pageview`
-       */
-      ga('set', 'page', to.fullPath)
-      ga('send', 'pageview')
-    })
+    ;(function () {
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-6MSWZRHBDD');
+    })()
   }
