2f5a7ed98f333aa9e2e77e337851b57e41088a05
razinkovnik <51551350+razinkovnik@users.noreply.github.com>
2021-01-17 22:35:28 +0300

    update

--- translit.py
+++ translit.py
@@ -1,9 +1,10 @@
+# %%
 from os import listdir
 from os.path import isfile, join
 import re
 import json
 
-mypath = r'D:\web\rnoar\static\docs'
+mypath = r'D:\web\rnoar\static\buf'
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 from transliterate import translit, get_available_language_codes
@@ -18,9 +19,9 @@ for ex in ["pdf", "doc", "rtf"]:
     urls = [url.replace(f"_{ex}", f".{ex}") for url in urls]
     names = [name.replace(f".{ex}", "") for name in names]
 
-folder = r'D:\web\rnoar\static\docs'
+
 for old_name, new_name in zip(onlyfiles, urls):
-    os.rename(os.path.join(folder, old_name), os.path.join(folder, new_name))
+    os.rename(os.path.join(mypath, old_name), os.path.join(mypath, new_name))
 
 urls = [f"/docs/{url}" for url in urls]
 
